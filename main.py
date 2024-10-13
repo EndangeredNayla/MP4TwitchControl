ac9bbeb777c23075ac9a086d0d99ba685658bc74
Nayla Hanegan <NaylaHanegan@gmail.com>
2024-10-12 21:03:39 -0400

    ciom

--- main.py
+++ main.py
@@ -10,51 +10,18 @@ import math
 class App(commands.Bot):
     def __init__(self, token, initial_channels):
         super().__init__(token="oauth:" + token, initial_channels=initial_channels, prefix="!")
-        self.client = twitchio.Client(token=token)
-        self.client.pubsub = pubsub.PubSubPool(self.client)
+        client = twitchio.Client(token=token)
+        client.pubsub = pubsub.PubSubPool(client)
 
         # Load configuration
         with open('config.json5', 'r') as config_file:
             self.config = pyjson5.load(config_file)
 
         # Register events
-        self.register_events()
+        self.register_events(client)
         dolphin_memory_engine.hook()
     
-    async def subscribe_to_topics(self):
-        topics = [
-            pubsub.channel_points(self.config["token"])[int(get_broadcaster_id(self.config["channelName"], self.config["token"]))],
-            pubsub.bits(self.config["token"])[int(get_broadcaster_id(self.config["channelName"], self.config["token"]))]
-        ]
-        await self.client.pubsub.subscribe_topics(topics)
-
-    
-    def register_events(self):
-        @self.event()
-        async def event_ready():
-            print(f'Logged in as | {self.nick}')
-            if "--create" in sys.argv:  # Check if the --create argument is present
-                for reward in self.config["rewards"]:
-                    if reward["enabled"] == "True":
-                        create_channel_point_reward(
-                            self.config["channelName"],
-                            reward["name"],
-                            reward["cost"],
-                            self.config["token"],
-                            reward["maxPerStream"],
-                            reward["maxPerUserPerStream"],
-                            reward["cooldown"],
-                            reward["maxPerStreamEnabled"],
-                            reward["imageSrc"]
-                        )
-            await self.subscribe_to_topics()
-
-        @self.event()
-        async def event_pubsub_bits(event: pubsub.PubSubBitsMessage):
-            print(f"Bits event: {event}")  # Log bits event
-            # do stuff on bit redemptions
-
-        @self.event()
+        @client.event()
         async def event_pubsub_channel_points(event: pubsub.PubSubChannelPointsMessage):
             print(f'Received channel points event: {event.reward.title}')  # Log the received event
             
@@ -84,56 +51,56 @@ class App(commands.Bot):
             if event.reward.title == self.config["rewards"][5]["name"]:  # -30 Coins P1
                 print('Triggering: -30 Coins P1')  # Log action
                 coinP1 = dolphin_memory_engine.read_bytes(0x8018FC54, 2)
-                thirtyLostCoinsP1 = int.from_bytes(maxTurns, byteorder='big') - 30
+                thirtyLostCoinsP1 = int.from_bytes(coinP1, byteorder='big') - 30
                 dolphin_memory_engine.write_bytes(0x8018FC54, thirtyLostCoinsP1.to_bytes(2, byteorder='big'))
                 print(f'New Coins P1: {thirtyLostCoinsP1}')  # Log new coin value
 
             if event.reward.title == self.config["rewards"][6]["name"]:  # -30 Coins P2
                 print('Triggering: -30 Coins P2')  # Log action
                 coinP2 = dolphin_memory_engine.read_bytes(0x8018FC84, 2)
-                thirtyLostCoinsP2 = int.from_bytes(maxTurns, byteorder='big') - 30
+                thirtyLostCoinsP2 = int.from_bytes(coinP2, byteorder='big') - 30
                 dolphin_memory_engine.write_bytes(0x8018FC54, thirtyLostCoinsP2.to_bytes(2, byteorder='big'))
                 print(f'New Coins P2: {thirtyLostCoinsP2}')  # Log new coin value
 
             if event.reward.title == self.config["rewards"][7]["name"]:  # -30 Coins P3
                 print('Triggering: -30 Coins P3')  # Log action
                 coinP3 = dolphin_memory_engine.read_bytes(0x8018FCB4, 2)
-                thirtyLostCoinsP3 = int.from_bytes(maxTurns, byteorder='big') - 30
+                thirtyLostCoinsP3 = int.from_bytes(coinP3, byteorder='big') - 30
                 dolphin_memory_engine.write_bytes(0x8018FC54, thirtyLostCoinsP3.to_bytes(2, byteorder='big'))
                 print(f'New Coins P3: {thirtyLostCoinsP3}')  # Log new coin value
 
             if event.reward.title == self.config["rewards"][8]["name"]:  # -30 Coins P4
                 print('Triggering: -30 Coins P4')  # Log action
                 coinP4 = dolphin_memory_engine.read_bytes(0x8018FC4, 2)
-                thirtyLostCoinsP4 = int.from_bytes(maxTurns, byteorder='big') - 30
+                thirtyLostCoinsP4 = int.from_bytes(coinP4, byteorder='big') - 30
                 dolphin_memory_engine.write_bytes(0x8018FC54, thirtyLostCoinsP4.to_bytes(2, byteorder='big'))
                 print(f'New Coins P4: {thirtyLostCoinsP4}')  # Log new coin value
 
             if event.reward.title == self.config["rewards"][9]["name"]:  # -1 Star P1
                 print('Triggering: -1 Star P1')  # Log action
                 coinP1 = dolphin_memory_engine.read_bytes(0x8018FC44, 2)
-                thirtyLostCoinsP1 = int.from_bytes(maxTurns, byteorder='big') - 1
+                thirtyLostCoinsP1 = int.from_bytes(coinP1, byteorder='big') - 1
                 dolphin_memory_engine.write_bytes(0x8018FC54, thirtyLostCoinsP1.to_bytes(2, byteorder='big'))
                 print(f'New Coins P1: {thirtyLostCoinsP1}')  # Log new coin value
 
             if event.reward.title == self.config["rewards"][10]["name"]:  # -1 Star P2
                 print('Triggering: -1 Star P2')  # Log action
                 coinP2 = dolphin_memory_engine.read_bytes(0x8018FC74, 2)
-                thirtyLostCoinsP2 = int.from_bytes(maxTurns, byteorder='big') - 1
+                thirtyLostCoinsP2 = int.from_bytes(coinP2, byteorder='big') - 1
                 dolphin_memory_engine.write_bytes(0x8018FC54, thirtyLostCoinsP2.to_bytes(2, byteorder='big'))
                 print(f'New Coins P2: {thirtyLostCoinsP2}')  # Log new coin value
 
             if event.reward.title == self.config["rewards"][11]["name"]:  # -1 Star P3
                 print('Triggering: -1 Star P3')  # Log action
                 coinP3 = dolphin_memory_engine.read_bytes(0x8018FCA4, 2)
-                thirtyLostCoinsP3 = int.from_bytes(maxTurns, byteorder='big') - 1
+                thirtyLostCoinsP3 = int.from_bytes(coinP3, byteorder='big') - 1
                 dolphin_memory_engine.write_bytes(0x8018FC54, thirtyLostCoinsP3.to_bytes(2, byteorder='big'))
                 print(f'New Coins P3: {thirtyLostCoinsP3}')  # Log new coin value
 
             if event.reward.title == self.config["rewards"][12]["name"]:  # -1 Star P4
                 print('Triggering: -1 Star P4')  # Log action
                 coinP4 = dolphin_memory_engine.read_bytes(0x8018FCD4, 2)
-                thirtyLostCoinsP4 = int.from_bytes(maxTurns, byteorder='big') - 1
+                thirtyLostCoinsP4 = int.from_bytes(coinP4, byteorder='big') - 1
                 dolphin_memory_engine.write_bytes(0x8018FC54, thirtyLostCoinsP4.to_bytes(2, byteorder='big'))
                 print(f'New Coins P4: {thirtyLostCoinsP4}')  # Log new coin value
 
@@ -168,10 +135,33 @@ class App(commands.Bot):
                 dolphin_memory_engine.write_bytes(0x8018FCA4, revParsed.to_bytes(2, byteorder='big'))
                 dolphin_memory_engine.write_bytes(0x8018FCD4, revParsed.to_bytes(2, byteorder='big'))
                 print(f'New Stars after Revolution: {revParsed}')  # Log new star value
+    
+    async def subscribe_to_topics(self, client):
+        topics = [
+            pubsub.channel_points(self.config["token"])[int(get_broadcaster_id(self.config["channelName"], self.config["token"]))],
+        ]
+        await client.pubsub.subscribe_topics(topics)    
 
-    @commands.command(name='hello')
-    async def hello_command(self, ctx):
-        await ctx.send(f'Hello, {ctx.author.name}!')
+
+    def register_events(self, client):
+        @self.event()
+        async def event_ready():
+            print(f'Logged in as | {self.nick}')
+            if "--create" in sys.argv:  # Check if the --create argument is present
+                for reward in self.config["rewards"]:
+                    if reward["enabled"] == "True":
+                        create_channel_point_reward(
+                            self.config["channelName"],
+                            reward["name"],
+                            reward["cost"],
+                            self.config["token"],
+                            reward["maxPerStream"],
+                            reward["maxPerUserPerStream"],
+                            reward["cooldown"],
+                            reward["maxPerStreamEnabled"],
+                            reward["imageSrc"]
+                        )
+            await self.subscribe_to_topics(client)
 
 if __name__ == "__main__":
     with open('config.json5', 'r') as config_file:
